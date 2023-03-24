2340bd03b91cd9908bcb24c0d3fb231818082a50
Nathan Atkinson <nathan@DESKTOP-DGCVKFO.localdomain>
2023-03-24 10:43:32 +0000

    add redis caching for geoserver requests

--- ckanext/geoserver_webservice/helpers.py
+++ ckanext/geoserver_webservice/helpers.py
@@ -1,20 +1,30 @@
 import uuid 
 import requests
+
 import logging
 from requests.auth import HTTPBasicAuth
+from requests_cache import CachedSession, RedisCache
 from ckan.common import config
 
 GEOSERVER_URL = config.get('ckanext.geoserver_webservice.url')
 GEOSERVER_USERNAME = config.get('ckanext.geoserver_webservice.username')
 GEOSERVER_PASSWORD = config.get('ckanext.geoserver_webservice.password')
 DEFAULT_ROLES = config.get('ckanext.geoserver_webservice.default_roles').split()
+REDIS_CONNECTION = config.get('ckan.redis.url', 'redis://localhost:6379/0')
+
 log = logging.getLogger(__name__)
+
+redis_host, redis_port = REDIS_CONNECTION.split('/')[2].split(':')
+backend = RedisCache(host=redis_host, port=redis_port)
+session = CachedSession('http_cache', backend=backend)
+session.settings.expire_after = 15
+session.settings.stale_if_error = True
 
 def get_geoserver_roles():
     try:
         basic = HTTPBasicAuth(GEOSERVER_USERNAME, GEOSERVER_PASSWORD)
         roles_url = f"{GEOSERVER_URL}/rest/security/roles.json"
-        response = requests.get(roles_url, auth=basic)
+        response = session.get(roles_url, auth=basic)
         if response.status_code != 200:
             log.error('Failed to fetch local geoserver role options')
             return []
