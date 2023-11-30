77cfa3d71519d5492e16c0e653036324573ad658
Nathan Atkinson <nathan@DESKTOP-DGCVKFO.localdomain>
2023-11-30 13:55:24 +0000

    bug where setting auth_obj in context would fail this is a core ckan issue that is being fixed in a later release. this problem only occurs within the geoserver_webservice api function as it uses the geoserver authkey to authenicate the user so once we verified the user authkey we would set the context as the owner of the authticated key. Instead we now just say that if the key is valid we pass ignore_auth:True to context

--- ckanext/geoserver_webservice/logic.py
+++ ckanext/geoserver_webservice/logic.py
@@ -102,6 +102,7 @@ def geoserver_webservice_user_roles_api_action(context, data_dict=None):
     Returns:
         The roles of a user
     """
+
     if 'user_id' not in data_dict.keys():
         raise tk.ValidationError(f"Bad request: Invalid request. Missing user_id parameter")
     user_id = data_dict.get('user_id')
@@ -142,7 +143,9 @@ def geoserver_webservice_api_action(context, data_dict=None):
     else:
         user = api_token.get_user_from_token(authkey)
     if user is not None:
-        roles = tk.get_action('get_geoserver_user_roles')({"user":user.id}, data_dict={"user_id":user.id})
+        context['ignore_auth'] = True
+        roles = tk.get_action('get_geoserver_user_roles')(context, data_dict={"user_id":user.id})
+        
         all_roles = [*set([*roles['user_roles'], *roles['organization_roles'], *roles['default_roles']])]
         role_str = ', '.join(all_roles)
         return {
