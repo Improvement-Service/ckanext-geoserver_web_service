b89a407898db74e9ec7192b0674f3cb6ff49d5bc
manojkumaris <manoj.kumar@improvementservice.org.uk>
2026-06-15 18:56:19 +0100

    changes to fetching geo roles

--- ckanext/geoserver_webservice/helpers.py
+++ ckanext/geoserver_webservice/helpers.py
@@ -6,10 +6,6 @@ from requests.auth import HTTPBasicAuth
 from requests_cache import CachedSession, RedisCache
 from ckan.common import config
 
-GEOSERVER_URL = config.get('ckanext.geoserver_webservice.url')
-GEOSERVER_USERNAME = config.get('ckanext.geoserver_webservice.username')
-GEOSERVER_PASSWORD = config.get('ckanext.geoserver_webservice.password')
-DEFAULT_ROLES = config.get('ckanext.geoserver_webservice.default_roles').split()
 REDIS_CONNECTION = config.get('ckan.redis.url', 'redis://localhost:6379/0')
 
 log = logging.getLogger(__name__)
@@ -21,22 +17,34 @@ session.settings.stale_if_error = True
 
 def get_geoserver_roles():
     try:
-        log.info("GEOSERVER_USERNAME-",GEOSERVER_USERNAME)
-        basic = HTTPBasicAuth(GEOSERVER_USERNAME, GEOSERVER_PASSWORD)
-        roles_url = f"{GEOSERVER_URL}/rest/security/roles.json"
-        response = session.get(roles_url, auth=basic)
-        if response.status_code != 200:
-            log.error('Failed to fetch local geoserver role options')
+        geoserver_url = config.get('ckanext.geoserver_webservice.url')
+        geoserver_username = config.get('ckanext.geoserver_webservice.username')
+        geoserver_password = config.get('ckanext.geoserver_webservice.password')
+        default_roles = config.get('ckanext.geoserver_webservice.default_roles', '').split()
+
+        log.info("GEOSERVER_USERNAME: %s", geoserver_username)
+        log.info("GEOSERVER_URL: %s", geoserver_url)
+
+        if not geoserver_url or not geoserver_username or not geoserver_password:
+            log.error("Missing GeoServer config values")
             return []
-        else:
-            all_roles = response.json().get('roles', [])
-            options_roles = [x[5:] for x in all_roles if x.startswith('ROLE_')]
-            options_roles = [x for x in options_roles if x not in DEFAULT_ROLES]
-            return options_roles
+
+        basic = HTTPBasicAuth(geoserver_username, geoserver_password)
+        roles_url = f"{geoserver_url}/rest/security/roles.json"
+        response = session.get(roles_url, auth=basic)
+
+        if response.status_code != 200:
+            log.error("Failed to fetch local geoserver role options")
+            return []
+
+        all_roles = response.json().get('roles', [])
+        options_roles = [x[5:] for x in all_roles if x.startswith('ROLE_')]
+        options_roles = [x for x in options_roles if x not in default_roles]
+        return options_roles
+
     except Exception as e:
-        log.error(e)
-        log.error('Failed to fetch local geoserver role options')
-    return []
+        log.exception("Failed to fetch local geoserver role options: %s", e)
+        return []
     
 def is_valid_uuid(val):
     try:
